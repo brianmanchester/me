b6915f64583022e1d446ac70d96f1949bf5a30ea
Brian Manchester <brian.l.manchester@gmail.com>
2020-12-20 11:18:56 -0800

    Revert

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
-      // jssStyles.parentElement?.removeChild(jssStyles);
+      jssStyles.parentElement?.removeChild(jssStyles);
     }
   }, []);
 
